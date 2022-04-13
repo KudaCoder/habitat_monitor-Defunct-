40d66bb89e6054ac68453b0c722894e6e24cf1f3
Warren Snowden <warren.snowden@gmail.com>
2022-04-14 00:23:22 +0100

    update monitor module to use new redis funcs and remove GPIO warnings

--- modules/monitor.py
+++ modules/monitor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from comm.redis import set_redis
+from comm.redis import update_redis
 from utility.dataclass import Reading
 from api.blueprints import utils as api_tools
 
@@ -14,6 +14,7 @@ DHTPin = 11
 class Monitor:
     def __init__(self):
         GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
                 
     def monitor_temp_hum(self):
         self.dht = DHT.DHT(DHTPin)
@@ -25,16 +26,21 @@ class Monitor:
                 if chk is self.dht.DHTLIB_OK:
                     break
                 time.sleep(0.1)
+            
+            temp = self.dht.temperature
+            hum = self.dht.humidity
 
-            reading = Reading(temp=self.dht.temperature, hum=self.dht.humidity)
-            set_redis("reading", reading)
+            update_redis("reading", {
+                "temp": temp, 
+                "hum": hum,
+            })
 
             # Save readings every 5 seconds
             end_time = time.time()
             delta = end_time - start_time
             if delta >= 5:
                 try:
-                    resp = api_tools.add_reading(temp=reading.temp, hum=reading.hum)
+                    resp = api_tools.add_reading(temp=temp, hum=hum)
                 except Exception:
                     pass
                 start_time = time.time()
