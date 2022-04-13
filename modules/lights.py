377de996bf7e66e578e70ea7044178a0cb68f818
Warren Snowden <warren.snowden@gmail.com>
2022-04-14 00:23:51 +0100

    try running heat lights at night time only

--- modules/lights.py
+++ modules/lights.py
@@ -1,4 +1,4 @@
-from comm.redis import get_redis
+from comm.redis import get_redis, update_redis
 
 import RPi.GPIO as GPIO
 
@@ -7,13 +7,12 @@ import threading
 import time
 
 NIGHT_LIGHT_PIN = 29
-WINTER_MONTHS = [1, 2, 3, 10, 11, 12]
-SUMMER_MONTHS = [4, 5, 6, 7, 8, 9]
 
 
 class Lights:
     def __init__(self):
         GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
         GPIO.setup(NIGHT_LIGHT_PIN, GPIO.OUT)
             
     def night_light_on(self):
@@ -38,11 +37,9 @@ class Lights:
             temp = reading.temp
 
             now = datetime.now()
-            month = int(now.strftime("%-m"))
             n_time = now.time()
             
             heat_on = False
-            # if month in WINTER_MONTHS:
             # Night time
             if (config.lights_off_time < n_time) or (n_time < config.lights_on_time):
                 if temp > config.night_h_sp:
@@ -53,24 +50,21 @@ class Lights:
                     heat_on = True
                     return_to_cool = False
             # Day time
-            elif config.lights_on_time <= n_time <= config.lights_off_time:
-                if temp > config.day_h_sp:
-                    return_to_cool = True
-                elif (config.day_l_sp <= temp <= config.day_h_sp) and not return_to_cool:
-                    heat_on = True
-                elif temp < config.day_l_sp:
-                    heat_on = True
-                    return_to_cool = False
+            # elif config.lights_on_time <= n_time <= config.lights_off_time:
+            #     if temp > config.day_h_sp:
+            #         return_to_cool = True
+            #     elif (config.day_l_sp <= temp <= config.day_h_sp) and not return_to_cool:
+            #         heat_on = True
+            #     elif temp < config.day_l_sp:
+            #         heat_on = True
+            #         return_to_cool = False
 
             if heat_on:
                 self.night_light_on()
+                update_redis("light", {"turned_on": True})
             else:
                 self.night_light_off()
+                update_redis("light", {"turned_on": False})
             
     def destroy(self):
         GPIO.cleanup()
-    
-    def start_lights(self):
-        thread = threading.Thread(target=self.control, daemon=True)
-        thread.start()
-
