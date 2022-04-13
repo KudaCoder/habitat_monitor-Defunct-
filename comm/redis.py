b1431f80d0fec9aaf3b7271a1f731b5bb97ada1f
Warren Snowden <warren.snowden@gmail.com>
2022-04-14 00:22:34 +0100

    refactor redis functions - needs better testing

--- comm/redis.py
+++ comm/redis.py
@@ -1,10 +1,21 @@
-import redis
-import pickle
 from datetime import datetime
+import pickle
+import redis
 
 from api.blueprints import utils as api_tools
-from utility.dataclass import Environment as data_env
 
+# Rename dataclasses to protect against any future 
+# scenario where models are used here as well
+from utility.dataclass import Environment as data_env
+from utility.dataclass import Reading as data_read
+from utility.dataclass import Light as data_light
+
+
+DC_LIBRARY = {
+    "environment": data_env,
+    "reading": data_read,
+    "light": data_light
+}
 
 class RedisWrapper:
     def connect(self):
@@ -12,18 +23,31 @@ class RedisWrapper:
         return redis.StrictRedis(connection_pool=connection_pool)
 
 
-def get_redis(key):
+def get_redis(key, reset=False):
     redis = RedisWrapper().connect()
     r = redis.get(key)
+
+    # If first time redis key is being set - only really appropriate to config
     if r is None:
+        data = None
         # TODO: Delete redis and see if this is working or not?
+        data = api_tools.new_config()
         if key == "environment":
-            config = api_tools.get_config()
-            if not config:
-                config = api_tools.new_config()
-            update_redis(key, config)
+            try:
+                if not reset:
+                    data = api_tools.get_config()
+                if reset or not data:
+                    data = api_tools.new_config()
+            except Exception:
+                pass
+
+        if data is not None:
+            update_redis(key, data)
             get_redis(key)
-            
+    
+    if r is None:
+        return r
+
     return pickle.loads(r)
 
 
@@ -33,17 +57,17 @@ def set_redis(key, obj):
 
 
 def update_redis(key, data):
-    r_data = None
+    dc = DC_LIBRARY.get(key)
+    if dc is None:
+        # TODO: Throw exception here?!
+        return
 
-    if key == "environment":
-        r_data = data_env()
-        for k, v in data.items():
-            if hasattr(r_data, k):
-                try:
-                    v = datetime.strptime(v, "%H:%M:%S").time()
-                except Exception as e:
-                    pass
-                setattr(r_data, k, v)
-        
-    if r_data is not None:
-        set_redis(key, r_data)
+    dc = dc()
+    for k, v in data.items():
+        if hasattr(dc, k):
+            try:
+                v = datetime.strptime(v, "%H:%M:%S").time()
+            except Exception:
+                pass
+            setattr(dc, k, v)
+    set_redis(key, dc)
